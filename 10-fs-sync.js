d58992393ebebf69fe6a3169530e109322e974c2
Tony Burks <tony.burks@ingramcontent.com>
2021-05-12 16:31:13 -0500

    first commit

---
+++ 10-fs-sync.js
@@ -0,0 +1,12 @@
+const { readFileSync, writeFileSync } = require('fs');
+console.log('start');
+const first = readFileSync('./content/first.txt', 'utf-8')
+const second = readFileSync('./content/second.txt', 'utf-8')
+
+writeFileSync(
+    './content/results-sync.txt', 
+    `Here is the result : ${first}, ${second}`,
+    { flag: 'a' }
+)
+console.log('done with this task');
+console.log('starting with the next line');
